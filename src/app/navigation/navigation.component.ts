8e26a38af669ad303e14713863f2f697f8e1ed4f
Jose Parra <joseparramabesoy@gmail.com>
2025-02-01 17:55:34 -0500

    agregando prime ng

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
   standalone: true,
-  imports: []
+  imports: [ButtonModule]
 })
 export class NavigationComponent {
   
