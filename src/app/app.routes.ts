3931b585df6709f6c7a329d6075fdb58a5aeac1c
jhon.ravelo <jhon.ravelo@zabud.com.co>
2025-02-03 08:45:35 -0500

    ajuste de styles y menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ export const routes: Routes = [
 
     { path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent) },
     { path: 'aspirantes', loadComponent: () => import('./features/aspirantes/aspirantes.component').then(m => m.AspirantesComponent) },
-    { path: 'navigation', loadComponent: () => import('./navigation/navigation.component').then(m => m.NavigationComponent) },
+    //{ path: 'navigation', loadComponent: () => import('./navigation/navigation.component').then(m => m.NavigationComponent) },
     { path: 'second', loadComponent: () => import('./second/second.component').then(m => m.SecondComponent) },
     { path: 'orden', loadComponent: () => import('./features/orden-evaluacion/orden-evaluacion.component').then(m => m.OrdenComponent) },
     { path: 'Omedica', loadComponent: () => import('./features/orden-medica/orden-medica.component').then(m => m.OrdenMedicaComponent) },
